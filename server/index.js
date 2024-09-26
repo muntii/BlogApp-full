f773ae38fb56772c991162d3abb3b88d4ba9718c
Muntajib <muntajibkhan14@gmail.com>
2024-09-26 21:51:41 +0500

    Update

--- server/index.js
+++ server/index.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 8800;
 dbConnection();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: ["https://blog-app-writer.vercel.app/"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
