61b9c87b39a59ac767a1d243219aac3cebea672a
Muntajib <muntajibkhan14@gmail.com>
2024-09-27 12:08:48 +0500

    Update

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use(helmet());
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin: https://blog-app-writer.vercel.app');
+  res.header('Access-Control-Allow-Origin: https://blog-app-client-steel.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
